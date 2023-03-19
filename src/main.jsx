d590b1cbaa39ded4a00ccabeffbee1756d272273
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-03-18 21:30:13 -0300

    Adicionando rotas para as telas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App'
 import './index.css'
+import Home from './pages/Home'
+import Form from './pages/Form'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='form' element={<Form />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
---
+++ src/pages/Form.jsx
@@ -0,0 +1,10 @@
+function Form() {
+  return (
+    <div>
+      <h1>TELA FORM</h1>
+      <p>Programador responsável: vinigay</p>
+    </div>            
+  );
+}
+
+export default Form
